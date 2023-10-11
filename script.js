f380a4f3fc1e36b6e45b7ed1c67eaac029614a1e
Asma Noor <asma@Asmas-MacBook-Air.local>
2023-10-11 10:23:59 -0500

    the wave

---
+++ script.js
@@ -0,0 +1,72 @@
+$(document).ready(function(){
+    console.log("ready");
+
+    
+
+    setInterval(doWave,milliseconds, move, div, );
+   
+    
+    function doWave(){
+        $(".move").each(function(){
+            $("div").animate({height: "HEIGHT" })
+    
+        });
+        $(".move").each(function(){
+            $("div").animate({height: "HEIGHT" })
+    
+        });
+        $(".move").each(function(){
+            $("div").animate({height: "HEIGHT" })
+    
+        });
+        $(".move").each(function(){
+            $("div").animate({height: "HEIGHT" })
+    
+        });
+        $(".move").each(function(){
+            $("div").animate({height: "HEIGHT" })
+    
+        });
+        $(".move").each(function(){
+            $("div").animate({height: "HEIGHT" })
+    
+        });
+        $(".move").each(function(){
+            $("div").animate({height: "HEIGHT" })
+    
+        });
+        $(".move").each(function(){
+            $("div").animate({height: "HEIGHT" })
+    
+        }); $(".move").each(function(){
+            $("div").animate({height: "HEIGHT" })
+    
+        });
+
+    }
+   
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
